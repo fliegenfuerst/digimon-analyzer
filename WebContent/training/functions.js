469e7e1757c60e9f714728ba76490b389a19b53e
fliegenfuerst <fliegenfuerst@gmail.com>
2017-05-09 18:52:07 +0200

    added slots to options

--- WebContent/training/functions.js
+++ WebContent/training/functions.js
@@ -17,7 +17,8 @@ var manual=1;
 var carrot=[1,1,1,1,1,1];
 var raddish=[1,1,1,1,1,1];
 var pine=1;
-function checkBoxClick(cb){
+var slots=1;
+function setBoost(cb){
 	var boost=1;
 	if (cb.checked){
 		boost=1.2;
@@ -55,11 +56,11 @@ function checkBoxClick(cb){
 	}
 	getTrainingValues();
 }
-function radioBtnClick(rb){
+function setTrainingItem(rb){
 	carrot=[1,1,1,1,1,1];
 	raddish=[1,1,1,1,1,1];
 	pine=1;
-	var rbList=document.getElementsByClassName("radioBtn");
+	var rbList=document.getElementsByClassName("trainingItem");
 	if (rb.checked===true){
 		for (var i=0;i<rbList.length;i++) {
 			rbList[i].checked=false;
@@ -76,6 +77,31 @@ function radioBtnClick(rb){
 				pine=1.5;
 				break;
 		}
+	}
+	getTrainingValues();
+}
+var slotRadioButtons=classGet("slots");
+function setSlots(rb){
+	for(var s=0;s<slotRadioButtons.length;s++){
+		slotRadioButtons[s].checked=false;
+	}
+	rb.checked=true;
+	switch(rb.id){
+		case "none":
+			slots=1;
+			break;
+		case "fail":
+			slots=0.5;
+			break;
+		case "same":
+			slots=2;
+			break;
+		case "other":
+			slots=4;
+			break;
+		case "gold":
+			slots=10;
+			break;
 	}
 	getTrainingValues();
 }
@@ -151,42 +177,42 @@ function getTrainingValues(){
 		}
 		resultArr[i]=Math.floor(multiplier*10)/10;
 	}
-	document.getElementById("hpHP").innerHTML=Math.floor(hpTrain[0]*raddish[0]*carrot[0]*pine*resultArr[0]);
-	document.getElementById("hpMP").innerHTML=Math.floor(hpTrain[1]*raddish[1]*carrot[1]*pine*resultArr[0]);
-	document.getElementById("hpOFF").innerHTML=Math.floor(hpTrain[2]*raddish[2]*carrot[2]*pine*resultArr[0]);
-	document.getElementById("hpDEF").innerHTML=Math.floor(hpTrain[3]*raddish[3]*carrot[3]*pine*resultArr[0]);
-	document.getElementById("hpSPD").innerHTML=Math.floor(hpTrain[4]*raddish[4]*carrot[4]*pine*resultArr[0]);
-	document.getElementById("hpBRN").innerHTML=Math.floor(hpTrain[5]*raddish[5]*carrot[5]*pine*resultArr[0]);
-	document.getElementById("mpHP").innerHTML=Math.floor(mpTrain[0]*raddish[0]*carrot[0]*pine*resultArr[1]);
-	document.getElementById("mpMP").innerHTML=Math.floor(mpTrain[1]*raddish[1]*carrot[1]*pine*resultArr[1]);
-	document.getElementById("mpOFF").innerHTML=Math.floor(mpTrain[2]*raddish[2]*carrot[2]*pine*resultArr[1]);
-	document.getElementById("mpDEF").innerHTML=Math.floor(mpTrain[3]*raddish[3]*carrot[3]*pine*resultArr[1]);
-	document.getElementById("mpSPD").innerHTML=Math.floor(mpTrain[4]*raddish[4]*carrot[4]*pine*resultArr[1]);
-	document.getElementById("mpBRN").innerHTML=Math.floor(mpTrain[5]*raddish[5]*carrot[5]*pine*resultArr[1]);
-	document.getElementById("offHP").innerHTML=Math.floor(offTrain[0]*raddish[0]*carrot[0]*pine*resultArr[2]);
-	document.getElementById("offMP").innerHTML=Math.floor(offTrain[1]*raddish[1]*carrot[1]*pine*resultArr[2]);
-	document.getElementById("offOFF").innerHTML=Math.floor(offTrain[2]*raddish[2]*carrot[2]*pine*resultArr[2]);
-	document.getElementById("offDEF").innerHTML=Math.floor(offTrain[3]*raddish[3]*carrot[3]*pine*resultArr[2]);
-	document.getElementById("offSPD").innerHTML=Math.floor(offTrain[4]*raddish[4]*carrot[4]*pine*resultArr[2]);
-	document.getElementById("offBRN").innerHTML=Math.floor(offTrain[5]*raddish[5]*carrot[5]*pine*resultArr[2]);
-	document.getElementById("defHP").innerHTML=Math.floor(defTrain[0]*raddish[0]*carrot[0]*pine*resultArr[3]);
-	document.getElementById("defMP").innerHTML=Math.floor(defTrain[1]*raddish[1]*carrot[1]*pine*resultArr[3]);
-	document.getElementById("defOFF").innerHTML=Math.floor(defTrain[2]*raddish[2]*carrot[2]*pine*resultArr[3]);
-	document.getElementById("defDEF").innerHTML=Math.floor(defTrain[3]*raddish[3]*carrot[3]*pine*resultArr[3]);
-	document.getElementById("defSPD").innerHTML=Math.floor(defTrain[4]*raddish[4]*carrot[4]*pine*resultArr[3]);
-	document.getElementById("defBRN").innerHTML=Math.floor(defTrain[5]*raddish[5]*carrot[5]*pine*resultArr[3]);
-	document.getElementById("spdHP").innerHTML=Math.floor(spdTrain[0]*raddish[0]*carrot[0]*pine*resultArr[4]);
-	document.getElementById("spdMP").innerHTML=Math.floor(spdTrain[1]*raddish[1]*carrot[1]*pine*resultArr[4]);
-	document.getElementById("spdOFF").innerHTML=Math.floor(spdTrain[2]*raddish[2]*carrot[2]*pine*resultArr[4]);
-	document.getElementById("spdDEF").innerHTML=Math.floor(spdTrain[3]*raddish[3]*carrot[3]*pine*resultArr[4]);
-	document.getElementById("spdSPD").innerHTML=Math.floor(spdTrain[4]*raddish[4]*carrot[4]*pine*resultArr[4]);
-	document.getElementById("spdBRN").innerHTML=Math.floor(spdTrain[5]*raddish[5]*carrot[5]*pine*resultArr[4]);
-	document.getElementById("brnHP").innerHTML=Math.floor(brnTrain[0]*raddish[0]*carrot[0]*pine*resultArr[5]);
-	document.getElementById("brnMP").innerHTML=Math.floor(brnTrain[1]*raddish[1]*carrot[1]*pine*resultArr[5]);
-	document.getElementById("brnOFF").innerHTML=Math.floor(brnTrain[2]*raddish[2]*carrot[2]*pine*resultArr[5]);
-	document.getElementById("brnDEF").innerHTML=Math.floor(brnTrain[3]*raddish[3]*carrot[3]*pine*resultArr[5]);
-	document.getElementById("brnSPD").innerHTML=Math.floor(brnTrain[4]*raddish[4]*carrot[4]*pine*resultArr[5]);
-	document.getElementById("brnBRN").innerHTML=Math.floor(brnTrain[5]*raddish[5]*carrot[5]*pine*resultArr[5]);
+	document.getElementById("hpHP").innerHTML=Math.floor(hpTrain[0]*raddish[0]*carrot[0]*pine*slots*resultArr[0]);
+	document.getElementById("hpMP").innerHTML=Math.floor(hpTrain[1]*raddish[1]*carrot[1]*pine*slots*resultArr[0]);
+	document.getElementById("hpOFF").innerHTML=Math.floor(hpTrain[2]*raddish[2]*carrot[2]*pine*slots*resultArr[0]);
+	document.getElementById("hpDEF").innerHTML=Math.floor(hpTrain[3]*raddish[3]*carrot[3]*pine*slots*resultArr[0]);
+	document.getElementById("hpSPD").innerHTML=Math.floor(hpTrain[4]*raddish[4]*carrot[4]*pine*slots*resultArr[0]);
+	document.getElementById("hpBRN").innerHTML=Math.floor(hpTrain[5]*raddish[5]*carrot[5]*pine*slots*resultArr[0]);
+	document.getElementById("mpHP").innerHTML=Math.floor(mpTrain[0]*raddish[0]*carrot[0]*pine*slots*resultArr[1]);
+	document.getElementById("mpMP").innerHTML=Math.floor(mpTrain[1]*raddish[1]*carrot[1]*pine*slots*resultArr[1]);
+	document.getElementById("mpOFF").innerHTML=Math.floor(mpTrain[2]*raddish[2]*carrot[2]*pine*slots*resultArr[1]);
+	document.getElementById("mpDEF").innerHTML=Math.floor(mpTrain[3]*raddish[3]*carrot[3]*pine*slots*resultArr[1]);
+	document.getElementById("mpSPD").innerHTML=Math.floor(mpTrain[4]*raddish[4]*carrot[4]*pine*slots*resultArr[1]);
+	document.getElementById("mpBRN").innerHTML=Math.floor(mpTrain[5]*raddish[5]*carrot[5]*pine*slots*resultArr[1]);
+	document.getElementById("offHP").innerHTML=Math.floor(offTrain[0]*raddish[0]*carrot[0]*pine*slots*resultArr[2]);
+	document.getElementById("offMP").innerHTML=Math.floor(offTrain[1]*raddish[1]*carrot[1]*pine*slots*resultArr[2]);
+	document.getElementById("offOFF").innerHTML=Math.floor(offTrain[2]*raddish[2]*carrot[2]*pine*slots*resultArr[2]);
+	document.getElementById("offDEF").innerHTML=Math.floor(offTrain[3]*raddish[3]*carrot[3]*pine*slots*resultArr[2]);
+	document.getElementById("offSPD").innerHTML=Math.floor(offTrain[4]*raddish[4]*carrot[4]*pine*slots*resultArr[2]);
+	document.getElementById("offBRN").innerHTML=Math.floor(offTrain[5]*raddish[5]*carrot[5]*pine*slots*resultArr[2]);
+	document.getElementById("defHP").innerHTML=Math.floor(defTrain[0]*raddish[0]*carrot[0]*pine*slots*resultArr[3]);
+	document.getElementById("defMP").innerHTML=Math.floor(defTrain[1]*raddish[1]*carrot[1]*pine*slots*resultArr[3]);
+	document.getElementById("defOFF").innerHTML=Math.floor(defTrain[2]*raddish[2]*carrot[2]*pine*slots*resultArr[3]);
+	document.getElementById("defDEF").innerHTML=Math.floor(defTrain[3]*raddish[3]*carrot[3]*pine*slots*resultArr[3]);
+	document.getElementById("defSPD").innerHTML=Math.floor(defTrain[4]*raddish[4]*carrot[4]*pine*slots*resultArr[3]);
+	document.getElementById("defBRN").innerHTML=Math.floor(defTrain[5]*raddish[5]*carrot[5]*pine*slots*resultArr[3]);
+	document.getElementById("spdHP").innerHTML=Math.floor(spdTrain[0]*raddish[0]*carrot[0]*pine*slots*resultArr[4]);
+	document.getElementById("spdMP").innerHTML=Math.floor(spdTrain[1]*raddish[1]*carrot[1]*pine*slots*resultArr[4]);
+	document.getElementById("spdOFF").innerHTML=Math.floor(spdTrain[2]*raddish[2]*carrot[2]*pine*slots*resultArr[4]);
+	document.getElementById("spdDEF").innerHTML=Math.floor(spdTrain[3]*raddish[3]*carrot[3]*pine*slots*resultArr[4]);
+	document.getElementById("spdSPD").innerHTML=Math.floor(spdTrain[4]*raddish[4]*carrot[4]*pine*slots*resultArr[4]);
+	document.getElementById("spdBRN").innerHTML=Math.floor(spdTrain[5]*raddish[5]*carrot[5]*pine*slots*resultArr[4]);
+	document.getElementById("brnHP").innerHTML=Math.floor(brnTrain[0]*raddish[0]*carrot[0]*pine*slots*resultArr[5]);
+	document.getElementById("brnMP").innerHTML=Math.floor(brnTrain[1]*raddish[1]*carrot[1]*pine*slots*resultArr[5]);
+	document.getElementById("brnOFF").innerHTML=Math.floor(brnTrain[2]*raddish[2]*carrot[2]*pine*slots*resultArr[5]);
+	document.getElementById("brnDEF").innerHTML=Math.floor(brnTrain[3]*raddish[3]*carrot[3]*pine*slots*resultArr[5]);
+	document.getElementById("brnSPD").innerHTML=Math.floor(brnTrain[4]*raddish[4]*carrot[4]*pine*slots*resultArr[5]);
+	document.getElementById("brnBRN").innerHTML=Math.floor(brnTrain[5]*raddish[5]*carrot[5]*pine*slots*resultArr[5]);
 }
 function getDigiDropDown(){
 	var div='';
@@ -261,4 +287,7 @@ function start(){
 		applyDigimon(obj);
 		document.getElementById('greenGym').checked=true;
 		extra=true;
+}
+function classGet(name){
+	return document.getElementsByClassName(name);
 }
